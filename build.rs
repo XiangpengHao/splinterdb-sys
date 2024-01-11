91e06de2d5797bd82555f1ac3c1bc7307253e58a
Xiangpeng <me@haoxp.xyz>
2024-01-11 10:47:49 -0600

    init

---
+++ build.rs
@@ -0,0 +1,50 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() {
+    let splinterdb_path = PathBuf::from("splinterdb");
+    env::set_var("CC", "clang");
+    env::set_var("LD", "clang");
+
+    Command::new("make")
+        .current_dir(&splinterdb_path)
+        .status()
+        .expect("Failed to build splinterdb");
+
+    let splinterdb_lib = splinterdb_path.join("build/release/lib");
+
+    println!("cargo:rustc-link-search={}", splinterdb_lib.display());
+
+    println!("cargo:rustc-link-lib=splinterdb");
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        .no_copy("splinterdb.*")
+        .no_copy("writable_buffer")
+        .no_copy("data_config")
+        .allowlist_type("splinterdb.*")
+        .allowlist_function("splinterdb.*")
+        .allowlist_function("default_data_config.*")
+        .allowlist_function("merge.*")
+        .allowlist_var("SPLINTERDB.*")
+        .allowlist_var(".*_SIZE")
+        .clang_arg("-DSPLINTERDB_PLATFORM_DIR=platform_linux")
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
